7196195e47179b88795b8ef6b049f789fa7ba52d
Denis Novikov <voothi@gmail.com>
2024-12-23 22:40:37 +0100

    refactor(token_extraction_spacy_minimal.py): streamline token extraction logic by simplifying lemmatization and updating commented CSV file name   feat(token_extraction_spacy_minimal.py): improve output ordering by mapping lemmatized tokens to their priority index from the new CSV file

--- token_extraction_spacy_minimal.py
+++ token_extraction_spacy_minimal.py
@@ -1,37 +1,27 @@
 import spacy
 import csv
 
-# Load the English NLP model
 nlp = spacy.load("en_core_web_lg")
 
-# Define the input text
 input_text = """Whatever you do, do not ask an AI to summarize the tips we're about to go through. 
 Because make no mistake, if you can't summarize information yourself and practice the summarization 
 of information, you're not going to be learning on your own. It's just that simple."""
 
-# Process the text with spaCy
 doc = nlp(input_text)
 
-# Lemmatize tokens and filter out non-alphabetic tokens, then convert to a set to remove duplicates
-unique_lemmatized_tokens = set(token.lemma_.lower() for token in doc if token.is_alpha)
+unique_lemmatized_tokens = set(token.lemma_ for token in doc if token.is_alpha)
 
-# Read the priority CSV file and create a dictionary for lemma indices
 lemma_index = {}
-with open("en-news-2023-1m-words.csv", "r", newline='', encoding='utf-8') as csvfile:
-    # Используем enumerate для отслеживания номера строки (начиная с 0)
+with open("en-wiki-priority.csv", "r", newline='', encoding='utf-8') as csvfile:
     for line_number, row in enumerate(csv.reader(csvfile)):
-        if row:  # Проверяем, что строка не пустая
-            word = row[0]  # Получаем слово и нормализуем его
-            # Сохраняем минимальный индекс для каждого слова
+        if row:
+            word = row[0]
             if word not in lemma_index or line_number < lemma_index[word]:
                 lemma_index[word] = line_number
 
-# Sort unique tokens based on their index in the priority CSV file
-# Используем float('inf') для слов, которых нет в словаре индексов
 sorted_unique_tokens = sorted(unique_lemmatized_tokens, 
                             key=lambda token: lemma_index.get(token, float('inf')))
 
-# Write data to a CSV file in priority order based on CSV file line number
 with open("tokens_spacy.csv", "w", newline='', encoding='utf-8') as csvfile:
     csv_writer = csv.writer(csvfile)
     for token in sorted_unique_tokens:

8737fa3f05645c56375e4d2afc80951e3ba3cf40
Denis Novikov <voothi@gmail.com>
2025-02-09 13:14:11 +0100

    20250209131409

--- token_extraction_spacy_minimal.de.py
+++ token_extraction_spacy_minimal.de.py
@@ -120,9 +120,9 @@ def process_text(input_text, output_file, sentence_context_size, detailed_output
                 # Write the row
                 original_form = token_to_original_form[token]
                 if two_column_output_to_file:
-                    tsv_writer.writerow([sentence, token, original_form, "", "", left_context, sentence, right_context, "", simple_list_entry, sentence, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "1", "", "", "", "", "", "", "1"])
+                    tsv_writer.writerow([token, original_form, "", "", left_context, sentence, right_context, "", simple_list_entry, sentence, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "1", "", "", "", "", "", "", "1"])
                 else:
-                    tsv_writer.writerow([sentence, token, "", "", "", left_context, sentence, right_context, "", simple_list_entry, sentence, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "1", "", "", "", "", "", "", "1"])
+                    tsv_writer.writerow([token,            "", "", "", left_context, sentence, right_context, "", simple_list_entry, sentence, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "1", "", "", "", "", "", "", "1"])
 
     # Output tokens in HTML table if html_output is enabled
     if html_output:

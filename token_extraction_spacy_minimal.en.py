c965d4e1fd29c55a7ffa0e5ed745b58127f45308
Denis Novikov <voothi@gmail.com>
2025-03-20 09:50:06 +0100

    202503200/95004

--- token_extraction_spacy_minimal.en.py
+++ token_extraction_spacy_minimal.en.py
@@ -17,7 +17,8 @@ def process_text(input_text, output_file, sentence_context_size, detailed_output
 
     # Create a lemma index from the CSV
     lemma_index = {}
-    with open("U:\\voothi\\20241223170748-token-extraction\\en-news-2023-1m-words.csv", "r", newline='', encoding='utf-8') as csvfile:
+    # with open("U:\\voothi\\20241223170748-token-extraction\\en-news-2023-1m-words.csv", "r", newline='', encoding='utf-8') as csvfile:
+    with open("U:\\voothi\\20241223170748-token-extraction\\en-wiki-priority.csv", "r", newline='', encoding='utf-8') as csvfile:
         for line_number, row in enumerate(csv.reader(csvfile)):
             if row:
                 word = row[0]

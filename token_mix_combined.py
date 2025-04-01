f9069808505288dba9dfdef19f46c6e1e50f07ca
Denis Novikov <voothi@gmail.com>
2025-04-01 22:35:00 +0200

    Refactor simple_list_entry formatting to handle line breaks more effectively

--- token_mix_combined.py
+++ token_mix_combined.py
@@ -338,11 +338,13 @@ def process_text_v1(
                     if include_simple_list:
                         # Если требуется, обработайте и заполните simple_list_entry
                         lemmas = process_sentence_lemmas(l1_sentence, lemma_index, nlp)
-                        simple_list_entry = "\n".join(lemmas)  # Пример заполнения
-
-                        # Заменяем \t на <br>, если задан ключ --with-br
                         if with_br:
-                            simple_list_entry = simple_list_entry.replace("\t", "<br>")
+                            # Добавляем <br> к каждой записи, кроме последней
+                            simple_list_entry = "<br>".join(lemmas[:-1]) + (
+                                "\n" + lemmas[-1] if len(lemmas) > 1 else ""
+                            )
+                        else:
+                            simple_list_entry = "\n".join(lemmas)
 
                     # Write the row
                     original_form = token_to_original_form[token]

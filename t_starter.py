ab41167799dc605398db78ba82dd12911c21ff23
Denis Novikov <voothi@gmail.com>
2025-08-26 11:31:25 +0200

    Add '--gcs-in-wordlist' argument to token extraction configuration

--- t_starter.py
+++ t_starter.py
@@ -33,6 +33,7 @@ def get_token_args(args, python_path, token_workspace):
         "--gcs",
         "--gcs-dictionary",
         "U:/voothi/20241223170748-token-extraction/20250826000433-test/german.dic",
+        "--gcs-in-wordlist",
         "--pipe"
     ]
 

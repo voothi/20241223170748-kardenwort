b84c4f4eba850d6ca5b5fdac823daeb174c9ce20
Denis Novikov <voothi@gmail.com>
2025-08-29 14:14:54 +0200

    20250829141452

--- t_starter.py
+++ t_starter.py
@@ -41,6 +41,8 @@ def get_token_args(args, python_path, token_workspace):
         "--gcs-dictionary",
         "U:/voothi/20241223170748-token-extraction/20250826000433-test/german.dic",
         "--gcs-in-wordlist",
+        "--lemma-override-file",
+        "U:/voothi/20241223170748-token-extraction/lemma_override.tsv",
         "--pipe"
     ]
 

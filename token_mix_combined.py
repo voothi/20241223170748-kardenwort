b08af49bdc7ca5e97adfc45ec2ba34aa1569de8f
Denis Novikov <voothi@gmail.com>
2025-08-31 23:58:07 +0200

    20250831235805

--- token_mix_combined.py
+++ token_mix_combined.py
@@ -149,7 +149,8 @@ def get_capitalized_lemma(token, spacy_lemma):
     if is_all_caps or has_internal_caps:
         return original_text
 
-    if token.pos_ in ["NOUN", "PROPN"]:
+    if (nlp.lang == 'de' and token.pos_ in ["NOUN", "PROPN"]) or \
+       (nlp.lang != 'de' and token.pos_ == "PROPN"):
         return spacy_lemma.capitalize()
 
     if token.is_sent_start and token.pos_ not in ["NOUN", "PROPN"]:

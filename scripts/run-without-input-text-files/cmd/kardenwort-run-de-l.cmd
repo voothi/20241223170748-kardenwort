73334703b45bd445fb3a687bf64b0133b8c77a27
Denis Novikov <voothi@gmail.com>
2025-09-10 20:03:13 +0200

    20250910200312

--- scripts/run-without-input-text-files/cmd/kardenwort-run-de-l.cmd
+++ scripts/run-without-input-text-files/cmd/kardenwort-run-de-l.cmd
@@ -8,7 +8,7 @@ set "KARDENWORT_INPUT_TEXT=%~1"
 
 rem Execute the Python script. It will read the environment variable internally.
 "U:/voothi/20250825231214-spacy-env/Scripts/python.exe" ^
-"U:/voothi/20241223170748-kardenwort/kardenwort.py" ^
+U:/voothi/20241223170748-kardenwort/kardenwort.py ^
 --type "word" ^
 --language "de" ^
 --lemma-index-file "U:/voothi/20241223170748-kardenwort/data/deu-mixed-typical-2011-1m-words.csv" ^

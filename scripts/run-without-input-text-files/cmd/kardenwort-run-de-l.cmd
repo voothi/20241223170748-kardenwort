a7050b17b29ca9e42e944434d53b7bd32067e4f7
Denis Novikov <voothi@gmail.com>
2025-09-10 17:49:21 +0200

    20250910174920

--- scripts/run-without-input-text-files/cmd/kardenwort-run-de-l.cmd
+++ scripts/run-without-input-text-files/cmd/kardenwort-run-de-l.cmd
@@ -2,12 +2,11 @@
 REM We set the UTF-8 encoding for correct work with Python.
 chcp 65001 > nul
 
-REM We run the python script, passing it the first argument (%1) from GoldenDict.
-U:/voothi/20250825231214-spacy-env/Scripts/python.exe ^
+REM We ECHO the argument from GoldenDict and PIPE it to the Python script's standard input.
+echo %~1 | U:/voothi/20250825231214-spacy-env/Scripts/python.exe ^
 U:/voothi/20241223170748-kardenwort/kardenwort.py ^
 --type "word" ^
 --language "de" ^
---text "%~1" ^
 --lemma-index-file "U:/voothi/20241223170748-kardenwort/data/deu-mixed-typical-2011-1m-words.csv" ^
 --lemma-override-file "U:/voothi/20241223170748-kardenwort/data/lemma_override_de.tsv" ^
 --de-dictionary-file "U:/voothi/20241223170748-kardenwort/data/german.dic" ^

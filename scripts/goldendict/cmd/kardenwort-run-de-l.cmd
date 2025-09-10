a89a89d130c6ad773e5266b1570a080968f1c8ab
Denis Novikov <voothi@gmail.com>
2025-09-10 17:17:03 +0200

    20250910171703

--- scripts/goldendict/cmd/kardenwort-run-de-l.cmd
+++ scripts/goldendict/cmd/kardenwort-run-de-l.cmd
@@ -7,10 +7,10 @@ U:/voothi/20250825231214-spacy-env/Scripts/python.exe ^
 U:/voothi/20241223170748-kardenwort/kardenwort.py ^
 --type "word" ^
 --language "de" ^
---text "%GDWORD%" ^
+--text "%1" ^
 --lemma-index-file "U:/voothi/20241223170748-kardenwort/data/deu-mixed-typical-2011-1m-words.csv" ^
---de-dictionary-file "U:/voothi/20241223170748-kardenwort/data/german.dic" ^
 --lemma-override-file "U:/voothi/20241223170748-kardenwort/data/lemma_override_de.tsv" ^
+--de-dictionary-file "U:/voothi/20241223170748-kardenwort/data/german.dic" ^
 --sentence-context-size "0" ^
 --stdout-format "html" ^
 --de-fix-genitive ^

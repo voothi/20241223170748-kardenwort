b243771a7135d080d8ddf20630362b744f896190
Denis Novikov <voothi@gmail.com>
2025-09-10 11:00:01 +0200

    20250910110000

--- kardenwort-runner.py
+++ kardenwort-runner.py
@@ -50,7 +50,7 @@ def get_script_args(args, python_path, workspace_path):
 
     base_args = [
         str(python_path),
-        str(workspace_path / "kardenkart.py"),
+        str(workspace_path / "kardenwort.py"),
         "--type", args.type,
         "--language", args.language,
         "--lemma-index-file", str(data_path / lemma_file),
